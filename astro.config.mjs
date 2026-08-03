5ba810859e574f7a35fe63fd91fef7fad1ec0f07
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-03 13:21:38 -0400

    add site configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Antonio",
+      cssVariable: "--font-antonio",
+      weights: [400],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Spartan",
+      cssVariable: "--font-spartan",
+      weights: [400, 700],
+    },
+  ],
+});
